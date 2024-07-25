13deb92bf2c834e3c8e3bfefa0f34b25adfeaf85
Gustavo Avide <gustavo@Gustavos-MacBook-Pro.local>
2024-07-25 19:42:45 +0100

    feat: using post list props

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import { DUMMY_POSTS } from '@/DUMMY_DATA'
 import PaddingContainer from '@/components/layout/padding-container'
 import PostCard from '@/components/post/post-card'
-import Image from 'next/image'
-import Link from 'next/link'
+import PostList from '@/components/post/post-lists'
 
 export default function Home() {
   return (
     <PaddingContainer>
       <main className='h-auto space-y-10'>
         <PostCard post={DUMMY_POSTS[0]}/>
-        <PostCard layout="vertical" post={DUMMY_POSTS[5]}/>
+        <PostList 
+          posts={DUMMY_POSTS.filter((post, index) => index > 0 && index < 4 && index !== 2)}/>
       </main>
     </PaddingContainer>
   )
