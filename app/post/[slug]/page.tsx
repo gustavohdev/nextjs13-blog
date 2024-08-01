33eda08eb378a46219c31d2a3605f9903125478a
Gustavo Avide <gustavo@Gustavos-MacBook-Pro.local>
2024-08-01 23:51:23 +0100

    feat:adding env variable and PostBody for the page

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { DUMMY_POSTS } from "@/DUMMY_DATA";
 import SocialLink from "@/components/elements/social-link";
 import PaddingContainer from "@/components/layout/padding-container";
+import PostBody from "@/components/post/post-body";
 import PostHero from "@/components/post/post-hero";
 import { notFound } from "next/navigation";
 
@@ -33,31 +34,32 @@ const Page = ({params}:{
                 <div className="sticky top-20 flex flex-col gap-5">
                     <SocialLink isSharedUrl plataform="facebook"
                         link={`https://www.facebook.com/sharer/sharer.php?u=
-                        ${'http://localhost:3000' + `/post/${post.slug}`}`
+                        ${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`
                     }
                     />
                     {/* have to use the link for twitter share */}
                     <SocialLink isSharedUrl plataform="twitter"
                         link={`https://twitter.com/intent/tweet?url=
-                        ${'http://localhost:3000' + `/post/${post.slug}`}`
+                        ${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`
                     }
                     />
                     {/* have to use the link for linkedin share */}
                     <SocialLink isSharedUrl plataform="linkedin"
                         link={`https://www.linkedin.com/shareArticle?mini=true&url=
-                        ${'http://localhost:3000' + `/post/${post.slug}`}`
+                        ${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`
                     }
                     />
                     {/* have to use the link for whatts app share */}
                     <SocialLink isSharedUrl plataform="whatsapp"
                         link={`https://api.whatsapp.com/send?text=
-                        ${'http://localhost:3000' + `/post/${post.slug}`}`
+                        ${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`
                     }
                 />
                 </div>
             </div>
-            <div className="h-[1200px] bg-slate-200 w-full">Post Body</div>
-
+            <div className="h-[1200px] bg-slate-200 w-full" >
+                <PostBody body={post.body}/>
+            </div>
         </div>
     </PaddingContainer>
   )
